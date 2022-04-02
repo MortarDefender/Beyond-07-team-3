b6127c8e812bdff8618573bbede1eba6e48395ab
Tamir Sror <tamirsr@mta.ac.il>
2022-04-03 01:25:06 +0300

    change in model
    Signed-off-by: Tamir Sror <tamirsr@mta.ac.il>

--- event_file/models.py
+++ event_file/models.py
@@ -1,11 +1,15 @@
+import os
 from django.db import models
-
 from events.models import EventParticipant
+
+
+def upload_to_function(instance, filename):
+    return os.path.join(f"files/{instance.participant_id.event_id.id}/", filename.split("/")[-1])
 
 
 class EventFile(models.Model):
     participant_id = models.ForeignKey(EventParticipant, on_delete=models.CASCADE)
-    file = models.FileField(upload_to="files")
+    file = models.FileField(upload_to=upload_to_function)
 
     def __str__(self) -> str:
         return f"{self.file.name}"
